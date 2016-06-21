faea36594fcd551299e83975640cbecc0f56ba84
benjamin <benjamin@niclabs.cl>
2016-06-21 17:35:10 -0400

    Test to turn right.

--- tests/test_direction.py
+++ tests/test_direction.py
@@ -6,11 +6,15 @@ RIGHT = -1
 
 
 class TestDirection(unittest.TestCase):
+    def setUp(self):
+        self.direction = Direction()
+
+    def tearDown(self):
+        self.direction = Direction()
 
     def test_create_default_direction(self):
-        default_direction = Direction()
-        self.assertEqual(default_direction.x, 1)
-        self.assertEqual(default_direction.y, 0)
+        self.assertEqual(self.direction.x, 1)
+        self.assertEqual(self.direction.y, 0)
 
     def test_create_direction(self):
         direction = Direction(0, 1)
@@ -21,10 +25,21 @@ class TestDirection(unittest.TestCase):
         self.assertRaises(UnProperDirectionException, Direction, 1, 1)
 
     def test_turn_left(self):
+        self.direction.turn(45, LEFT)
+        self.assertAlmostEqual(self.direction.x, (2.0 ** (1.0 / 2) / 2))
+        self.assertAlmostEqual(self.direction.y, (2.0 ** (1.0 / 2) / 2))
+        self.direction.turn(45, LEFT)
+        self.assertAlmostEqual(self.direction.x, 0)
+        self.assertAlmostEqual(self.direction.y, 1)
+
+    def test_turn_right(self):
+        self.direction.turn(45, RIGHT)
+        self.assertAlmostEqual(self.direction.x, (2.0 ** (1.0 / 2) / 2))
+        self.assertAlmostEqual(self.direction.y, -(2.0 ** (1.0 / 2) / 2))
+        self.direction.turn(45, RIGHT)
+        self.assertAlmostEqual(self.direction.x, 0)
+        self.assertAlmostEqual(self.direction.y, -1)
+
+    def test_equal_directions(self):
         direction = Direction()
-        direction.turn(45, LEFT)
-        self.assertAlmostEqual(direction.x, (2.0 ** (1.0 / 2) / 2))
-        self.assertAlmostEqual(direction.y, (2.0 ** (1.0 / 2) / 2))
-        direction.turn(45, LEFT)
-        self.assertAlmostEqual(direction.x, 0)
-        self.assertAlmostEqual(direction.y, 1)
+        self.assertEqual(direction, Direction())
