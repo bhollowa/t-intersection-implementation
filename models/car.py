b4e7cea36af8c3f31eca5012e429a41fd78bac42
benjamin <benjamin@niclabs.cl>
2016-06-21 17:41:47 -0400

    Modify car to have direction. Delete other functions that didn't considered it.

--- models/car.py
+++ models/car.py
@@ -1,31 +1,12 @@
+from models.direction import Direction
+
+
 class Car:
+    max_speed = 17  # In meters/seconds
+    acceleration = 3  # In meters/seconds*seconds
 
-    def __init__(self):
-        self.pos_x = 0
-        self.pos_y = 0
-        self.speed = 0
-        self.max_speed = 17
-        self.acceleration = 3
-
-    def reset(self):
-        self.pos_x = 0
-        self.pos_y = 0
-        self.speed = 0
-
-    def accelerate_x_seconds(self, seconds):
-        total_distance = self.speed*seconds + self.acceleration * (seconds ** 2) / 2
-        self.pos_x += total_distance
-        self.speed += self.acceleration * seconds
-        return total_distance
-
-    def accelerate_y_seconds(self, seconds):
-        total_distance = self.speed * seconds + self.acceleration * (seconds ** 2) / 2
-        self.pos_y += total_distance
-        self.speed += self.acceleration * seconds
-        return total_distance
-
-    def accelerate_x_meters(self, meters):
-        total_time = (-self.speed + (self.speed**2 + 2*self.acceleration*meters)**(1.0/2))/self.acceleration
-        self.pos_x += meters
-        self.speed += self.acceleration * total_time
-        return total_time
+    def __init__(self, pos_x=0, pos_y=0, absolute_speed=0, direction=Direction()):
+        self.pos_x = pos_x
+        self.pos_y = pos_y
+        self.absolute_speed = absolute_speed
+        self.direction = direction
