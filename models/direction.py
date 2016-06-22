2d0dc4a210662e87a9109fc11b3693d1a464987e
benjamin <benjamin@niclabs.cl>
2016-06-22 14:36:16 -0400

    Fyx float comparison by casting them to int.

--- models/direction.py
+++ models/direction.py
@@ -3,13 +3,13 @@ from math import cos, sin, degrees as d, radians, atan
 
 class Direction:
 
-    def __init__(self, x=1, y=0):
+    def __init__(self, x=1.0, y=0.0):
         """
         Initializer for a direction of an object viewed from top. The sum of the squares values of x and y must be 1.
         :param x: Value of the x axis.
         :param y: Value of the x axis.
         """
-        if x**2+y**2 != 1:
+        if int(x**2 + y**2) != int(1.0):
             raise UnProperDirectionException
         self.x = x
         self.y = y
