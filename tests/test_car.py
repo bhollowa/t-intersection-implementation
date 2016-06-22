cfe09bc9b7fba38d4a9350747ef5b142aec9ff26
benjamin <benjamin@niclabs.cl>
2016-06-22 16:38:21 -0400

    SECONDS obtain from the car class. Create test for brake deceleration.

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,14 +1,13 @@
 import unittest
-from models.car import Car, ExceedCarMaximumSpeedError
+from models.car import Car, ExceedCarMaximumSpeedError, StopSpeedReached
 from models.direction import Direction
-
-SECONDS = 1000.0
 
 
 class TestCar(unittest.TestCase):
 
     def setUp(self):
         self.car = Car()
+        self.degrees = 2.0 ** (1.0 / 2) / 2.0
 
     def test_create_car(self):
 
@@ -16,66 +15,84 @@ class TestCar(unittest.TestCase):
         self.assertEqual(self.car.pos_y, 0)
         self.assertEqual(self.car.absolute_speed, 0)
         self.assertEqual(self.car.direction, Direction())
-        self.assertEqual(self.car.max_absolute_speed, 17)
-        self.assertEqual(self.car.acceleration, 3)
+        self.assertEqual(self.car.max_absolute_speed, 120.0)
+        self.assertEqual(self.car.acceleration_rate, 3)
 
         new_car = Car(50, 50, 17, Direction(0, -1))
         self.assertEqual(new_car.pos_x, 50)
         self.assertEqual(new_car.pos_y, 50)
         self.assertEqual(new_car.absolute_speed, 17)
         self.assertEqual(new_car.direction, Direction(0, -1))
-        self.assertRaises(ExceedCarMaximumSpeedError, Car, 0, 0, 30)
+        self.assertRaises(ExceedCarMaximumSpeedError, Car, 0, 0, 121)
 
     def test_move_car(self):
 
-        self.car.move(5.0, SECONDS)
+        self.car.move(5.0, self.car.SECONDS)
         self.assertEqual(self.car.pos_x, 0)
         self.assertEqual(self.car.pos_y, 0)
 
         new_car = Car(absolute_speed=17)
-        new_car.move(5.0, SECONDS)
+        new_car.move(5.0, new_car.SECONDS)
         self.assertEqual(new_car.pos_x, 85)
         self.assertEqual(new_car.pos_y, 0)
 
         new_car = Car(absolute_speed=10, direction=Direction(-1, 0))
-        new_car.move(5.0, SECONDS)
+        new_car.move(5.0, new_car.SECONDS)
         self.assertEqual(new_car.pos_x, -50)
         self.assertEqual(new_car.pos_y, 0)
 
-        degrees = 2.0 ** (1.0 / 2) / 2.0
-        new_car = Car(absolute_speed=10, direction=Direction(degrees, -degrees))
-        new_car.move(5.0, SECONDS)
-        new_pos = degrees * 5.0 * 10
+        new_car = Car(absolute_speed=10, direction=Direction(self.degrees, -self.degrees))
+        new_car.move(5.0, new_car.SECONDS)
+        new_pos = self.degrees * 5.0 * 10
         self.assertEqual(new_car.pos_x, new_pos)
         self.assertEqual(new_car.pos_y, -new_pos)
 
     def test_accelerate_car(self):
 
-        self.car.accelerate(1.0, SECONDS)
+        self.car.accelerate(1.0, self.car.SECONDS)
         self.assertEqual(self.car.absolute_speed, 3)
         self.assertEqual(self.car.pos_x, 3.0 / 2.0)
         self.assertEqual(self.car.pos_y, 0)
 
-        self.car.accelerate(1.0, SECONDS)
+        self.car.accelerate(1.0, self.car.SECONDS)
         self.assertEqual(self.car.absolute_speed, 6)
         self.assertEqual(self.car.pos_x, 6)
         self.assertEqual(self.car.pos_y, 0)
 
-        degrees = 2.0 ** (1.0 / 2) / 2.0
-        new_car = Car(direction=Direction(degrees, degrees))
-        new_car.accelerate(5.0, SECONDS)
+        new_car = Car(direction=Direction(self.degrees, self.degrees))
+        new_car.accelerate(5.0, new_car.SECONDS)
         self.assertEqual(new_car.absolute_speed, 15)
-        self.assertEqual(new_car.pos_x, 75.0 / 2.0 * degrees)
-        self.assertEqual(new_car.pos_y, 75.0 / 2.0 * degrees)
+        self.assertEqual(new_car.pos_x, 75.0 / 2.0 * self.degrees)
+        self.assertEqual(new_car.pos_y, 75.0 / 2.0 * self.degrees)
 
-        new_car = Car(direction=Direction(-degrees, -degrees))
-        new_car.accelerate(5.0, SECONDS)
+        new_car = Car(direction=Direction(-self.degrees, -self.degrees))
+        new_car.accelerate(5.0, new_car.SECONDS)
         self.assertEqual(new_car.absolute_speed, 15)
-        self.assertEqual(new_car.pos_x, - 75.0 / 2.0 * degrees)
-        self.assertEqual(new_car.pos_y, - 75.0 / 2.0 * degrees)
+        self.assertEqual(new_car.pos_x, - 75.0 / 2.0 * self.degrees)
+        self.assertEqual(new_car.pos_y, - 75.0 / 2.0 * self.degrees)
 
-        self.assertRaises(ExceedCarMaximumSpeedError, new_car.accelerate, 10, SECONDS)
+        self.assertRaises(ExceedCarMaximumSpeedError, new_car.accelerate, 40, new_car.SECONDS)
 
+    def test_decelerate_car(self):
+
+        self.assertRaises(StopSpeedReached, self.car.brake_decelerate, 1, self.car.SECONDS)
+
+        new_car = Car(absolute_speed=30)
+        new_car.brake_decelerate(1, new_car.SECONDS)
+        self.assertEqual(new_car.absolute_speed, 26)
+        self.assertEqual(new_car.pos_x, 28)
+        self.assertEqual(new_car.pos_y, 0)
+
+        new_car.brake_decelerate(5, new_car.SECONDS)
+        self.assertEqual(new_car.absolute_speed, 6)
+        self.assertEqual(new_car.pos_x, 108)
+        self.assertEqual(new_car.pos_y, 0)
+
+        new_car = Car(direction=Direction(-self.degrees, -self.degrees), absolute_speed=30)
+        new_car.brake_decelerate(7, new_car.SECONDS)
+        self.assertEqual(new_car.absolute_speed, 2)
+        self.assertEqual(new_car.pos_x, - 112.0 * self.degrees)
+        self.assertEqual(new_car.pos_y, - 112.0 * self.degrees)
 
 if __name__ == '__main__':
     unittest.main()
