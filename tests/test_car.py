65b7a7db798def34ca0bcbadb8f342562e2988ff
benjamin <benjamin@niclabs.cl>
2016-06-29 17:35:31 -0400

    Add test for a turning car.

--- tests/test_car.py
+++ tests/test_car.py
@@ -1,6 +1,8 @@
 import unittest
-from models.car import Car, ExceedCarMaximumSpeedError, StopSpeedReached
+from models.car import Car, ExceedCarMaximumSpeedError, StopSpeedReached, ExceedTurningSpeedException, \
+    ExceedWheelTurningException
 from models.direction import Direction
+from math import pi
 
 
 class TestCar(unittest.TestCase):
@@ -41,7 +43,7 @@ class TestCar(unittest.TestCase):
         self.assertEqual(new_car.pos_x, -50)
         self.assertEqual(new_car.pos_y, 0)
 
-        new_car = Car(absolute_speed=10, direction=Direction(self.degrees, -self.degrees))
+        new_car = Car(absolute_speed=10, direction=Direction(self.degrees, -self.degrees, -45))
         new_car.move(5.0, new_car.SECONDS)
         new_pos = self.degrees * 5.0 * 10
         self.assertEqual(new_car.pos_x, new_pos)
@@ -59,13 +61,13 @@ class TestCar(unittest.TestCase):
         self.assertEqual(self.car.pos_x, 6)
         self.assertEqual(self.car.pos_y, 0)
 
-        new_car = Car(direction=Direction(self.degrees, self.degrees))
+        new_car = Car(direction=Direction(self.degrees, self.degrees, 45))
         new_car.accelerate(5.0, new_car.SECONDS)
         self.assertEqual(new_car.absolute_speed, 15)
         self.assertEqual(new_car.pos_x, 75.0 / 2.0 * self.degrees)
         self.assertEqual(new_car.pos_y, 75.0 / 2.0 * self.degrees)
 
-        new_car = Car(direction=Direction(-self.degrees, -self.degrees))
+        new_car = Car(direction=Direction(-self.degrees, -self.degrees, 225))
         new_car.accelerate(5.0, new_car.SECONDS)
         self.assertEqual(new_car.absolute_speed, 15)
         self.assertEqual(new_car.pos_x, - 75.0 / 2.0 * self.degrees)
@@ -88,7 +90,7 @@ class TestCar(unittest.TestCase):
         self.assertEqual(new_car.pos_x, 108)
         self.assertEqual(new_car.pos_y, 0)
 
-        new_car = Car(direction=Direction(-self.degrees, -self.degrees), absolute_speed=30)
+        new_car = Car(direction=Direction(-self.degrees, -self.degrees, 225), absolute_speed=30)
         new_car.brake_decelerate(7, new_car.SECONDS)
         self.assertEqual(new_car.absolute_speed, 2)
         self.assertEqual(new_car.pos_x, - 112.0 * self.degrees)
@@ -103,6 +105,67 @@ class TestCar(unittest.TestCase):
         self.assertEqual(self.car.pos_x, 162)
         self.assertEqual(self.car.pos_y, 0)
 
+    def test_turn_car(self):
+
+        self.car.turn(1, self.car.SECONDS, 45)
+        self.assertEqual(self.car.absolute_speed, 0)
+        self.assertEqual(self.car.direction, Direction())
+        self.assertEqual(self.car.pos_x, 0)
+        self.assertEqual(self.car.pos_y, 0)
+
+        self.car.accelerate(5, self.car.SECONDS)
+        self.assertRaises(ExceedWheelTurningException, self.car.turn, 5, self.car.SECONDS, 90)
+        self.assertRaises(ExceedTurningSpeedException, self.car.turn, 5, self.car.SECONDS, 45)
+
+        new_car = Car(absolute_speed=10)
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, 4, 1)
+        self.assertAlmostEqual(new_car.pos_y, 4, 1)
+        self.assertAlmostEqual(new_car.direction.x, 0, 1)
+        self.assertAlmostEqual(new_car.direction.y, 1, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, 0, 1)
+        self.assertAlmostEqual(new_car.pos_y, 8, 1)
+        self.assertAlmostEqual(new_car.direction.x, -1, 1)
+        self.assertAlmostEqual(new_car.direction.y, 0, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, -4, 1)
+        self.assertAlmostEqual(new_car.pos_y, 4, 1)
+        self.assertAlmostEqual(new_car.direction.x, 0, 1)
+        self.assertAlmostEqual(new_car.direction.y, -1, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, 0, 1)
+        self.assertAlmostEqual(new_car.pos_y, 0, 1)
+        self.assertAlmostEqual(new_car.direction.x, 1, 1)
+        self.assertAlmostEqual(new_car.direction.y, 0, 1)
+
+        new_car = Car(absolute_speed=10, direction=Direction(0, 1, 90))
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, -4, 1)
+        self.assertAlmostEqual(new_car.pos_y, 4, 1)
+        self.assertAlmostEqual(new_car.direction.x, -1, 1)
+        self.assertAlmostEqual(new_car.direction.y, 0, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, -8, 1)
+        self.assertAlmostEqual(new_car.pos_y, 0, 1)
+        self.assertAlmostEqual(new_car.direction.x, 0, 1)
+        self.assertAlmostEqual(new_car.direction.y, -1, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, -4, 1)
+        self.assertAlmostEqual(new_car.pos_y, -4, 1)
+        self.assertAlmostEqual(new_car.direction.x, 1, 1)
+        self.assertAlmostEqual(new_car.direction.y, 0, 1)
+
+        new_car.turn(0.627, self.car.SECONDS, 45)
+        self.assertAlmostEqual(new_car.pos_x, 0, 1)
+        self.assertAlmostEqual(new_car.pos_y, 0, 1)
+        self.assertAlmostEqual(new_car.direction.x, 0, 1)
+        self.assertAlmostEqual(new_car.direction.y, 1, 1)
 
 if __name__ == '__main__':
     unittest.main()
