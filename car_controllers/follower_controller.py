926a4f08fa2938e316f7a596bcc92ebf4f5341df
bhollowa <benjamin.hollo.s@gmail.com>
2016-09-23 20:48:47 -0300

    Adjust headway (intersection length + car length)

--- car_controllers/follower_controller.py
+++ car_controllers/follower_controller.py
@@ -2,15 +2,15 @@ from car_controllers.deafult_controller import default_controller
 
 
 def follower_controller(inputs, car):
-    headway = 200
+    headway = 280
     r, l, u, d = inputs
     if not car.follow:
         car.set_controller(default_controller)
     if car.get_message() is not None:
         if car.distance_to_center() - car.get_message().distance_to_center() < headway:
             car.set_speed(car.get_message().speed - 10)
-            return r, l, u, d - 10
+            return r, l, u, d - 2
         elif car.distance_to_center() - car.get_message().distance_to_center() < headway*3/2:
             car.set_speed(car.get_message().speed)
-            return r, l, u, d - 5
-    return r, l, u + 12, d
+            return r, l, u, d - 1
+    return r, l, u + 1, d
