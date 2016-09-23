a62bbe3a55d4d3a2ad9aaaab869d8092df0f8246
bhollowa <benjamin.hollo.s@gmail.com>
2016-09-23 20:22:18 -0300

    Pep things

--- car_controllers/follower_controller.py
+++ car_controllers/follower_controller.py
@@ -8,9 +8,9 @@ def follower_controller(inputs, car):
         car.set_controller(default_controller)
     if car.get_message() is not None:
         if car.distance_to_center() - car.get_message().distance_to_center() < headway:
-            car.set_speed(car.get_message().speed - 5)
-            return r, l, u, d -15
+            car.set_speed(car.get_message().speed - 10)
+            return r, l, u, d - 10
         elif car.distance_to_center() - car.get_message().distance_to_center() < headway*3/2:
             car.set_speed(car.get_message().speed)
-            return r, l, u, d
+            return r, l, u, d - 5
     return r, l, u + 12, d
