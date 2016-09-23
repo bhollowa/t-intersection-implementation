ffcefb442f6411eea70d2be9253dd2270db055e2
bhollowa <benjamin.hollo.s@gmail.com>
2016-09-23 20:09:02 -0300

    Create function to display car name and following car. Move functions to auxiliar function file.

--- auxiliar_functions.py
+++ auxiliar_functions.py
@@ -1,5 +1,12 @@
 from pygame.locals import *
 import pygame
+from models.car import Car
+from random import randint
+
+
+white = (255,255,255)
+black = (0,0,0)
+initial_positions = [(430, 700, 0, 1), (760, 350, 90, 2), (350, 10, 180, 3), (10, 430, 270, 4)]
 
 
 def distance_to_center(**kwargs):
@@ -31,3 +38,24 @@ def check_close_application(user_input):
             if event.key == K_ESCAPE:
                 return False
     return True
+
+
+def random_car( name):
+    pos_x, pos_y, direction, lane = initial_positions[randint(0, len(initial_positions) - 1)]
+    initial_speed = randint(0, 30)
+    return Car(str(name), pos_x, pos_y, direction=direction, lane=lane, absolute_speed=initial_speed)
+
+
+def colliding_cars(car_list):
+    for i in range(len(car_list)):
+        for j in range(i+1,len(car_list)):
+            if car_list[i].screen_car.colliderect(car_list[j].screen_car):
+                return True
+
+
+def display_name_and_followers(rect, display, letter):
+    display.blit(letter.render(rect.name, True, black), rect.get_position())
+    followers = rect.get_followers()
+    for follower in followers:
+        x, y = follower.get_position()
+        display.blit(letter.render(rect.name, True, black), (x+30, y))
--- main.py
+++ main.py
@@ -1,43 +1,35 @@
-import pygame
-from models.car import Car
-from car_controllers.supervisor_level import supervisor_level
-from random import randint
-from models.auxiliar_functions import check_close_application
 import os
+import pygame
+
+from auxiliar_functions import check_close_application, random_car, colliding_cars, display_name_and_followers
+from car_controllers.supervisor_level import supervisor_level
 
 images_directory = os.getcwd() + "/images/"
 screen = pygame.display.set_mode((768, 768))
 bg = pygame.image.load(images_directory + "background.jpg")
-initial_positions = [(430, 700, 0, 1), (760, 350, 90, 2), (350, 10, 180, 3), (10, 430, 270, 4)]
 new_cars = []
 cars = []
 clock = pygame.time.Clock()
 inputs = (0, 0, 0, 0)
 iteration = True
 new_car = False
-BLACK = (0, 0, 0)
+
 FPS = 60
 counter = 0
 screen_cars = []
-
-
-def random_car(config, name):
-    pos_x, pos_y, direction, lane = config
-    initial_speed = randint(0, 30)
-    return Car("auto numero " + str(name / 50), pos_x, pos_y, direction=direction, lane=lane, absolute_speed=initial_speed)
-
-def colliding_cars(car_list):
-    for i in range(len(car_list)):
-        for j in range(i+1,len(car_list)):
-            if car_list[i].screen_car.colliderect(car_list[j].screen_car):
-                return True
+car_name_counter = 0
 
 
 if __name__ == "__main__":
+    cars_per_second = 2
+    pygame.init()
+    font = pygame.font.SysFont('Arial', 20)
+    pygame.display.set_caption('Car simulation')
     while iteration:
         counter += 1
-        if counter % 35 == 0:
-            new_cars.append(random_car(initial_positions[randint(0, len(initial_positions) - 1)], counter))
+        if counter % (60/cars_per_second) == 0:
+            new_cars.append(random_car(car_name_counter))
+            car_name_counter += 1
             new_car = True
         if new_car:
             new_cars = supervisor_level(new_cars, cars)
@@ -54,10 +46,11 @@ if __name__ == "__main__":
             car.update(*inputs)
             screen_cars.append(car.screen_car)
             screen.blit(car.rotated_image, car.screen_car)
+            display_name_and_followers(car, screen, font)
             pygame.display.update()
             inputs = (0, 0, 0, 0)
         screen.blit(bg, (0, 0))
         screen_cars = []
         if colliding_cars(cars):
-            pygame.time.wait(1000)
+            pygame.time.wait(3000)
         clock.tick(FPS)
