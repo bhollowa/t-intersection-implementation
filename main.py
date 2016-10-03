d2e940fc347f1ffc82015146e9c438c8576be53c
bhollowa <benjamin.hollo.s@gmail.com>
2016-10-03 11:42:32 -0300

    Move some variables. Move all log fase to a single if. Use function to abotain loggers.

--- main.py
+++ main.py
@@ -1,32 +1,27 @@
 import os
 import pygame
-from auxiliar_functions import check_close_application, random_car, colliding_cars, display_info_on_car, setup_logger
+from auxiliar_functions import check_close_application, random_car, colliding_cars, display_info_on_car, create_logs
 from car_controllers.supervisor_level import supervisor_level
 from sys import argv
-import logging
 
 
-def main_simulation(log_name, graphic_environment, limit):
+def main_simulation(graphic_environment, limit, **kwargs):
     wait = "wait" in argv
     images_directory = os.path.dirname(os.path.abspath(__file__)) + "/images/"
-    logger_directory = os.path.dirname(os.path.abspath(__file__)) + "/logs/"
-    setup_logger("collision", logger_directory + "collisions" + log_name + ".log")
-    setup_logger("numbers_of_cars", logger_directory + "total_cars" + log_name + ".log")
-    setup_logger("left_intersection", logger_directory + "left_intersection" + log_name + ".log")
-    collision_log = logging.getLogger('collision')
-    total_cars_log = logging.getLogger('numbers_of_cars')
-    left_intersection_log = logging.getLogger('left_intersection')
+    if "log" in kwargs:
+        print "logs creados"
+        collision_log, left_intersection_log, total_cars_log = create_logs(kwargs["log"])
 
     new_cars = []
     cars = []
     iteration = True
     new_car = False
     intersection = pygame.Rect(280, 280, 210, 210)
-    FPS = 60
     collisions = 0
     collision_list = []
 
     if graphic_environment:
+        fps = 60
         screen = pygame.display.set_mode((768, 768))
         screen_rect = screen.get_rect()
         bg = pygame.image.load(images_directory + "background.jpg")
@@ -39,11 +34,12 @@ def main_simulation(log_name, graphic_environment, limit):
     counter = 0
     car_name_counter = 0
     cars_per_second = 2
+    collision = False
+    collision_message = ""
+    left_intersection_cars = []
+
     while iteration and car_name_counter < limit:
         counter += 1
-        if car_name_counter % 250 == 0 and counter % (60/cars_per_second) == 0:
-            message = '{"cars_simulated":' + str(car_name_counter) + '}'
-            total_cars_log.info(message)
         if counter % (60/cars_per_second) == 0:
             if len(cars) > 0:
                 new_cars.append(random_car(car_name_counter, 20, last_lane=cars[len(cars)-1].get_lane()))
@@ -62,7 +58,7 @@ def main_simulation(log_name, graphic_environment, limit):
             if not car.screen_car.colliderect(screen_rect):
                 cars.remove(car)
                 car.set_left_intersection_time()
-                left_intersection_log.info(str(car))
+                left_intersection_cars.append(car)
                 for follower in car.get_followers():
                     follower.stop_following()
                 continue
@@ -75,25 +71,39 @@ def main_simulation(log_name, graphic_environment, limit):
             code = str(collided_cars[0].get_name()) + "to" + str(
                 collided_cars[1].get_name())
             if code not in collision_list:
+                collision = True
                 collision_list.append(code)
                 collisions += 1
-                message = '{"collision_code":"' + code + '",'
-                message += ' "collision_initial_conditions":['
+                collision_message = '{"collision_code":"' + code + '", "collision_initial_conditions":['
                 for car in cars:
-                    message += car.to_json() + ','
-                message = message[:len(message)-1] + '],'
-                message += '"collided_cars":['
+                    collision_message += car.to_json() + ','
+                collision_message = collision_message[:len(collision_message)-1] + '],"collided_cars":['
                 for car in collided_cars:
-                    message += car.to_json() + ','
-                    if graphic_environment and wait:
-                        pygame.time.wait(10000)
-                message = message[:len(message) - 1] + ']}'
-                collision_log.info(message)
+                    collision_message += car.to_json() + ','
+                collision_message = collision_message[:len(collision_message) - 1] + ']}'
+                if graphic_environment and wait:
+                    pygame.time.wait(10000)
+        if "log" in kwargs:
+            if collision:
+                collision_log.info(collision_message)
+                collision = False
+                collision_message = ""
+            if car_name_counter % 250 == 0 and counter % (60 / cars_per_second) == 0:
+                message = '{"cars_simulated":' + str(car_name_counter) + '}'
+                total_cars_log.info(message)
+            log_message = ""
+            for car in left_intersection_cars:
+                log_message += '[' + str(car) + ','
+            left_intersection_cars = []
+            if len(log_message) > 0:
+                log_message = log_message[:len(log_message)-1] + ']'
+                left_intersection_log.info(log_message)
         if graphic_environment:
             pygame.display.update(screen_rect)
-            clock.tick(FPS)
+            clock.tick(fps)
+
     print "Last records. Total collisions: " + str(collisions)
 
-car_limit = 10000
-main_simulation("_no_graphic", False, car_limit)
-main_simulation("_graphic", True, car_limit)
+car_limit = 250
+main_simulation(False, car_limit, log="_no_graphic")
+main_simulation(True, car_limit, log="_graphic")
