6f92359bb1d2a7a19dd88d2124423da9c9e029b7
bhollowa <benjamin.hollo.s@gmail.com>
2016-10-04 15:12:57 -0300

    Supervisory level sets the new_car variable of a car to False.

--- car_controllers/supervisor_level.py
+++ car_controllers/supervisor_level.py
@@ -12,9 +12,9 @@ def supervisor_level(new_cars, old_cars, attack=False):
     :param new_cars: list of new cars in the intersection
     :param old_cars: old cars in the intersection
     :param attack: if true, all cars will be set default controller, driving at maximum speed.
-    :return: an empty list.
     """
     for new_car in new_cars:
+        new_car.set_old()
         if not attack:
             for i in range(len(old_cars)):
                 if new_car.cross_path(old_cars[len(old_cars)-(i+1)]):
@@ -29,4 +29,3 @@ def supervisor_level(new_cars, old_cars, attack=False):
         else:
             old_cars.append(new_car)
             new_car.set_controller(default_controller)
-    return []
