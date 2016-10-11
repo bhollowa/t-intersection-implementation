40a04ba0a6d38f787135b59e93302d3d709d238b
bhollowa <benjamin.hollo.s@gmail.com>
2016-10-11 14:17:43 -0300

    Create variable for continue access of array.

--- car_controllers/supervisor_level.py
+++ car_controllers/supervisor_level.py
@@ -17,8 +17,9 @@ def supervisor_level(new_cars, old_cars, attack=False):
         new_car.set_old()
         if not attack:
             for i in range(len(old_cars)):
-                if new_car.cross_path(old_cars[len(old_cars)-(i+1)]):
-                    old_cars[len(old_cars) - (i + 1)].add_follower(new_car)
+                other_car = old_cars[len(old_cars)-(i+1)]
+                if new_car.cross_path(other_car):
+                    other_car.add_follower(new_car)
                     new_car.set_controller(follower_controller)
                     old_cars.append(new_car)
                     new_car.start_following()
