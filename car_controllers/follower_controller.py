7073c03a94d4c433c6ad78d6ccc55f37f874f9dc
bhollowa <benjamin.hollo.s@gmail.com>
2016-10-20 15:03:50 -0300

    Controller now calculates the control law and by that adjust acceleration.

--- car_controllers/follower_controller.py
+++ car_controllers/follower_controller.py
@@ -6,22 +6,22 @@ def follower_controller(car):
     Controller of a car that is following some other. If the car is more than 3/2 headway distance to the center than
     the distance to the center of its following car, the car will accelerate, otherwise it will stay at the following
     car speed.
-    :param inputs: inputs of the car.
     :param car: car to bre controlled
     :return: new inputs of the car.
     """
-    headway = car.get_car_length() * 3 + 0.5 * car.get_speed()
+    headway_time = 0.7
+    k_p = 0.2
+    k_d = 0.7
+    stand_still_distance = car.get_car_length() * 4
     if not car.is_following():
         car.set_controller(default_controller)
     car_message = car.get_following_car_message()
     if car_message is not None:
-        distance_difference = car_message.virtual_distance() - car.virtual_distance()
-        if distance_difference < car.get_car_length():
-            car.set_speed(0)
-        if distance_difference < headway:
-            # car.set_speed(car_message.speed - 10)
-            return -20, 0
-        elif distance_difference < headway*3/2:
-            # car.set_speed(car_message.speed)
-            return 10, 0
-    return 15, 0
+        virtual_distance = car_message.virtual_distance() - car.virtual_distance()
+        value_1 = -1 * car.get_control_law_value() + car_message.get_acceleration()
+        value_2 = k_p * (virtual_distance - stand_still_distance - headway_time * car.get_speed())
+        value_3 = k_d * ((virtual_distance - car.get_last_virtual_distance()) - headway_time * car.get_acceleration())
+        control_law_derivative = (value_1 + value_2 + value_3) / headway_time
+        car.set_last_virtual_distance(virtual_distance)
+        car.set_control_law_value((car.get_acceleration() + control_law_derivative)%3)
+        car.set_acceleration(car.get_acceleration() + control_law_derivative)
