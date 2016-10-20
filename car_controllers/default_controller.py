b1b096e3ba32f0db4804b2b2048106e7ac8c0839
bhollowa <benjamin.hollo.s@gmail.com>
2016-10-20 15:59:53 -0300

    Controllers make the cars turn if they have that intention.

--- car_controllers/default_controller.py
+++ car_controllers/default_controller.py
@@ -5,3 +5,11 @@ def default_controller(car):
     :return: inputs to move the car.
     """
     car.set_acceleration(3)
+    if car.get_intention() == "r":
+        if car.virtual_distance() >= 248:
+            if car.get_direction_variation() < 90:
+                car.set_direction(car.get_direction() - 15)
+    elif car.get_intention() == "l":
+        if car.virtual_distance() >= 281:
+            if car.get_direction_variation() < 90:
+                car.set_direction(car.get_direction() + 15)
--- car_controllers/follower_controller.py
+++ car_controllers/follower_controller.py
@@ -13,6 +13,14 @@ def follower_controller(car):
     k_p = 0.2
     k_d = 0.7
     stand_still_distance = car.get_car_length() * 4
+    if car.get_intention() == "r":
+        if car.virtual_distance() >= 248:
+            if car.get_direction_variation() < 90:
+                car.set_direction(car.get_direction() - 15)
+    elif car.get_intention() == "l":
+        if car.virtual_distance() >= 281:
+            if car.get_direction_variation() < 90:
+                car.set_direction(car.get_direction() + 15)
     if not car.is_following():
         car.set_controller(default_controller)
     car_message = car.get_following_car_message()
